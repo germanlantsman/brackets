a4d5a9ef1d6441a211f089f57b8da714bfc639a0
germanlantsman <germanlantsman@gmail.com>
2023-02-16 23:08:15 +0300

    feat: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2 != 0) { return false };
+  let i;  
+
+  for (i = 0; i < str.length; i += 1) {
+    bracketsConfig.forEach(elem => {
+      if (str[i] === elem[0] && str[i + 1] === elem[1]) {
+        str = str.slice(0, i) + str.slice(i + 2, str.length);
+        i = i - 2;
+      }
+    })
+
+  }
+  return (i === 0);
 }
